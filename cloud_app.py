27c188842f35063b3f01c93970edc8e8cd45403e
viliam-gago <60181358+viliam-gago@users.noreply.github.com>
2022-06-29 13:05:09 +0200

    Add files via upload

--- cloud_app.py
+++ cloud_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import altair as alt
-import sqlalchemy
-import pymysql
+# import sqlalchemy
+# import pymysql
 import pandas as pd
-import plotly.express as px
+# import plotly.express as px
 
 
 
